52c9119cb689291b8558d4c66409d1b5eb32ec34
kkzhang <k@z-pk.com>
2013-12-18 09:45:03 +0800

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
-from fabric.operations import local
+from fabric.context_managers import cd
+from fabric.operations import local, run
+from fabric.state import env
 
 __author__ = 'patrickz'
 
+env.hosts=['115.29.41.2']
+env.user = 'root'
+env.key_filename = '~/.ssh/id_rsa.pub'
+
 def push():
     local('git add .; git commit; git push origin master')
+
+def update():
+    with cd('/opt/pkgs/king_downloader'):
+        run('git pull')
+
+def deploy():
+    push()
+    update()
